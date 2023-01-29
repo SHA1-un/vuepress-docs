bd329f3cd9f8fda303a597a3bc70db38574df4f6
Shaun <shaun@gatekeeper.technology>
2023-01-29 08:46:17 +0200

    initial commit

---
+++ src/.vuepress/.temp/auto-catalog/config.js
@@ -0,0 +1,23 @@
+import { defineClientConfig } from "@vuepress/client";
+import { defineAsyncComponent, h } from "vue";
+import { hasGlobalComponent } from "/home/shaun/Documents/Personal/VuePress/vuepress_theme_hope/node_modules/vuepress-shared/lib/client/index.js";
+
+export default defineClientConfig({
+  enhance: ({ app }) => {
+    if (!hasGlobalComponent("AutoCatalog", app))
+      app.component("AutoCatalog", () =>
+        h(
+          defineAsyncComponent(
+            () =>
+              import("/home/shaun/Documents/Personal/VuePress/vuepress_theme_hope/node_modules/vuepress-plugin-components/lib/client/components/Catalog.js")
+          ),
+          {
+            titleGetter: (meta) => meta["t"],
+            iconGetter: (meta) => meta["i"],
+            orderGetter: (meta) => meta["O"],
+            shouldIndex: (meta) => Boolean(meta["I"]),
+          }
+        )
+      );
+  },
+});
